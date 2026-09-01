13c611632064d5b1fe2464f02f40d42be41035da
Echevarria01 <ma.echeverria@alumno.um.edu.ar>
2026-09-01 12:13:57 -0300

    login

---
+++ admin.js
@@ -0,0 +1,27 @@
+import { auth } from "./firebase.js";
+
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+const btn = document.getElementById("loginBtn");
+const error = document.getElementById("error");
+
+btn.addEventListener("click", async () => {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+
+    await signInWithEmailAndPassword(auth, email, password);
+
+    window.location.href = "panel.html";
+
+  } catch {
+
+    error.textContent = "Correo o contraseña incorrectos";
+
+  }
+
+});
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhHOjeRPhbgp3_IZ1KoaIrc1hZwq_gViQ",
+  authDomain: "friki-impre3d.firebaseapp.com",
+  projectId: "friki-impre3d",
+  storageBucket: "friki-impre3d.firebasestorage.app",
+  messagingSenderId: "149466029112",
+  appId: "1:149466029112:web:e5bf8dc40d2a1b1da65acd"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
